61307261e13a3438d65286ff9b047d1134975289
meliodas113 <anupam.fas27@gmail.com>
2025-05-20 22:36:24 +0530

    files added changes:wq

--- src/components/hooks/useFetchPayments.js
+++ src/components/hooks/useFetchPayments.js
@@ -9,15 +9,12 @@ export const useFetchPayments = () => {
     const fetchPayouts = async () => {
       setLoading(true);
       try {
-        const response = await fetch(
-          "https://868b44bc-5e39-488f-9751-fa8fcaacbb53.mock.pstmn.io/payouts",
-          {
-            method: "GET",
-            headers: {
-              "x-api-key": "",
-            },
-          }
-        );
+        const response = await fetch("", {
+          method: "GET",
+          headers: {
+            "x-api-key": "",
+          },
+        });
 
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
